706d7a25a5057a934eaa35c73b0e53bb35a46819
Евгений Филиппов <filippov.ewgen@gmail.com>
2020-05-05 14:09:33 +0300

    Initialization

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Home;
