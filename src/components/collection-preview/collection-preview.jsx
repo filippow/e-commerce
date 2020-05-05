6aa99e1357c9f7b26570d2aa24b43a4f2cbb5c21
Евгений Филиппов <filippov.ewgen@gmail.com>
2020-05-06 00:17:04 +0300

    Add ShopPage that displays information about goods in a market

---
+++ src/components/collection-preview/collection-preview.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './collection-preview.scss';
+
+import CollectionItem from "../collection-item/collection-item";
+
+
+const CollectionPreview = ({title, items}) => {
+    return (
+        <div className='collection-preview'>
+            <h1 className='collection-title'>{title}</h1>
+            <div className='preview'>
+                {
+                    items
+                        .slice(0, 4)
+                        .map(({id, ...props}) => (
+                            <CollectionItem
+                                key={id}
+                                {...props}
+                            />
+                        ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default CollectionPreview;
