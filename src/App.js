a3338b8ba4d3e523e4f3e29967b7aa6ad49b6ce9
Евгений Филиппов <filippov.ewgen@gmail.com>
2020-05-05 22:29:57 +0300

    Add Router for Hat Items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './App.scss';
 
 import Home from "./pages/homepage/Home";
 
+
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+)
+
+const HatsItem = () => (
+    <div>
+        <h1>HATS ITEM</h1>
+    </div>
+)
+
 function App() {
-  return (
-      <div>
-        <Home/>
-      </div>
-  )
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route path='/hats/:id' component={HatsItem}/>
+                <Route path='/hats' component={HatsPage}/>
+            </Switch>
+        </div>
+    )
 }
 
 export default App;
