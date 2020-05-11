ab5f91c6be4d9383e13deced17525583451650fd
Евгений Филиппов <filippov.ewgen@gmail.com>
2020-05-11 14:54:31 +0300

    Added ActionTypes for Header Component and Redirect Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import './App.scss';
 
 import Header from "./components/header/Header";
@@ -44,14 +44,22 @@ class App extends React.Component {
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage}/>
-                <Route path='/auth' component={Auth}/>
+                <Route exact path='/auth' render={this.renderAuthPage}/>
             </Switch>
         </div>
     )
+
+    renderAuthPage = () => {
+        return this.props.currentUser ? <Redirect to='/'/> : <Auth/>;
+    }
 }
+
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
